c57b68891a20f88cadc467e3167172544fd27798
gilles951 <gilles.pelegrin@telenet.be>
2016-04-05 16:35:57 +0200

    serial usb connection added

--- src/pingpong/Window.java
+++ src/pingpong/Window.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.LinkedList;
 import javax.swing.Timer;
+import pingpong.hardware.Data_input;
 import pingpong.math.Sensor;
 import pingpong.visual.*;
 
@@ -25,12 +26,7 @@ public class Window extends javax.swing.JFrame {
     private Sensor s1,s2,s3;
     private LinkedList<Sensor> sensorList;
     private Timer timer;
-
-    
-    
-    
-    
-    
+    private Data_input serialCom;
     
     private ActionListener taskPerformer = new ActionListener() {
   
@@ -46,14 +42,12 @@ public class Window extends javax.swing.JFrame {
          
          graph1.update(s1.getRadius(), timer.getDelay());
          graph2.update(s2.getRadius(), timer.getDelay());
-         graph3.update(s3.getRadius(), timer.getDelay());
-         
-         
+         graph3.update(s3.getRadius(), timer.getDelay());        
+
         }
     };
     
-    
-    
+   
     public Window() {
         initComponents();
         sensorList = new LinkedList();
@@ -76,8 +70,8 @@ public class Window extends javax.swing.JFrame {
        timer = new Timer(100 ,taskPerformer);
        timer.setRepeats(true);
        timer.start();
-
-
+       
+       serialCom = new Data_input();
     }
 
     /**
@@ -97,9 +91,13 @@ public class Window extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         BaudRate = new javax.swing.JLabel();
         infoTitel = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        BithDepth = new javax.swing.JLabel();
-        jComboBox2 = new javax.swing.JComboBox<>();
+        baudRateList = new javax.swing.JComboBox<>();
+        comList = new javax.swing.JComboBox<>();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        SearchComButton = new javax.swing.JButton();
+        connectButton = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
@@ -176,11 +174,29 @@ public class Window extends javax.swing.JFrame {
         infoTitel.setOpaque(true);
         infoTitel.setRequestFocusEnabled(false);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "9600", "14400", "19200", "28800", "38400", "56000", "57600", "115200" }));
+        baudRateList.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "9600", "14400", "19200", "28800", "38400", "56000", "57600", "115200" }));
 
-        BithDepth.setText("bit depth");
+        comList.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "No Com" }));
 
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "8", "10" }));
+        jLabel3.setText("Bith depht: 8 bit");
+
+        jLabel4.setText("Pariteit bit:  NONE");
+
+        jLabel5.setText("Stop bit: 1 bit");
+
+        SearchComButton.setText("Search Com");
+        SearchComButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SearchComButtonActionPerformed(evt);
+            }
+        });
+
+        connectButton.setText("Connect");
+        connectButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                connectButtonActionPerformed(evt);
+            }
+        });
 
         jMenu1.setText("File");
 
@@ -217,14 +233,20 @@ public class Window extends javax.swing.JFrame {
                         .addComponent(infoTitel, javax.swing.GroupLayout.PREFERRED_SIZE, 77, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(61, 61, 61)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(BaudRate)
-                            .addComponent(BithDepth))
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(45, Short.MAX_VALUE))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(BaudRate)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(baudRateList, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(SearchComButton)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(comList, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(connectButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(15, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -244,18 +266,27 @@ public class Window extends javax.swing.JFrame {
                                 .addGap(2, 2, 2)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(graphPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(graphPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(graphPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(13, 13, 13)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                     .addComponent(BaudRate)
-                                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGap(7, 7, 7)
+                                    .addComponent(baudRateList, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel3)
+                                .addGap(8, 8, 8)
+                                .addComponent(jLabel5)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabel4)
+                                .addGap(18, 18, 18)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(BithDepth)
-                                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                        .addGap(18, 18, 18)
-                        .addComponent(graphPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(SearchComButton)
+                                    .addComponent(comList, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addComponent(connectButton)))
                         .addGap(18, 18, 18)
                         .addComponent(graphPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -265,6 +296,27 @@ public class Window extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void SearchComButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SearchComButtonActionPerformed
+        LinkedList<String> comPortList = new LinkedList();
+        comPortList = serialCom.getComPort();
+        comList.removeAllItems();
+        
+        if (comPortList.size() == 0) {
+          comList.addItem("No Com");
+        } else {
+            for (int i = 0; i<comPortList.size(); i++){
+                comList.addItem(comPortList.get(i));
+            }
+        } 
+    }//GEN-LAST:event_SearchComButtonActionPerformed
+
+    private void connectButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_connectButtonActionPerformed
+
+        serialCom.changeBaud(Integer.parseInt(baudRateList.getSelectedItem().toString()));
+        serialCom.setComPoort(comList.getSelectedItem().toString());
+        serialCom.init();
+    }//GEN-LAST:event_connectButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -310,15 +362,19 @@ public class Window extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel AwsomeTitel;
     private javax.swing.JLabel BaudRate;
-    private javax.swing.JLabel BithDepth;
     private javax.swing.JPanel PalletPanel;
+    private javax.swing.JButton SearchComButton;
+    private javax.swing.JComboBox<String> baudRateList;
+    private javax.swing.JComboBox<String> comList;
+    private javax.swing.JButton connectButton;
     private javax.swing.JPanel graphPanel1;
     private javax.swing.JPanel graphPanel2;
     private javax.swing.JPanel graphPanel3;
     private javax.swing.JLabel infoTitel;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
